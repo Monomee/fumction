243f1e480e12e2f2609d20037a9d29a1835c12d6
Monomee <hoangthth12@gmail.com>
2024-11-28 22:11:24 +0700

    count

---
+++ count/count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count
+{
+    internal class Program
+    {   
+        static int countChar(string s, char c)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c) { count++; }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            Console.WriteLine("Enter a char to count:");
+            char c = Char.Parse(Console.ReadLine());
+            Console.WriteLine("Count: "+ countChar(str, c));
+            Console.ReadLine();
+        }
+    }
+}
